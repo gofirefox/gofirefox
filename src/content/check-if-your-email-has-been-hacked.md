47303d3ba60eb090dbed22240c11d328bea96736
Satvik Jagannath <satvik.jagannath7@gmail.com>
2020-04-23 13:06:58 +0530

    you have been hacked

---
+++ src/content/check-if-your-email-has-been-hacked.md
@@ -0,0 +1,30 @@
+---
+layout: post
+title: "Check if your email has been hacked"
+author: Satvik Jagannath
+tags: ["Web"]
+image: img/hacked.jpg
+date: "2020-04-23T12:45:00.000Z"
+draft: false
+---
+
+Have you been hacked or are is your email been hacked?
+See if you’ve been part of an online data breach.
+
+Find out what hackers already know about you. Learn how to stay a step ahead of them.
+
+## What exactly is a data breach?
+
+A data breach happens when personal or private information of us gets stolen without permission because of a lot of reasons - it may be because of a hacker taking away your data, poor security of webapps/database, employee or others stealing it, etc., A data breach can also happen by accident like if someone's login credentials accidentally get posted publicly in places like GitHub or Pastebin etc.,
+
+## How to check if you have been hacked?
+
+- ### [Firefox Monitor](https://monitor.firefox.com/)
+
+- ### [haveibeenpwned](https://haveibeenpwned.com/)
+
+## Will Firefox Monitor save you from a Hack?
+
+No! Firefox or any company will not prevent data breaches from happening. They alert you from breaches that affect you as soon as they happen, so you can take the next step of protecting or fighting quickly. If a breach involved financial information, you'll also need to monitor your own financial accounts and credit reports for anything suspicious.
+
+Take care folks! Stay safe :)
--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,15 +1,15 @@
-import { graphql } from 'gatsby';
-import * as React from 'react';
-import { css } from '@emotion/core';
-import Helmet from 'react-helmet';
+import { graphql } from "gatsby"
+import * as React from "react"
+import { css } from "@emotion/core"
+import Helmet from "react-helmet"
 
-import Footer from '../components/Footer';
-import SiteNav from '../components/header/SiteNav';
-import PostCard from '../components/PostCard';
-import Wrapper from '../components/Wrapper';
-import IndexLayout from '../layouts';
-import config from '../website-config';
-import Pagination from '../components/Pagination';
+import Footer from "../components/Footer"
+import SiteNav from "../components/header/SiteNav"
+import PostCard from "../components/PostCard"
+import Wrapper from "../components/Wrapper"
+import IndexLayout from "../layouts"
+import config from "../website-config"
+import Pagination from "../components/Pagination"
 
 import {
   inner,
@@ -21,8 +21,8 @@ import {
   SiteHeaderContent,
   SiteMain,
   SiteTitle,
-} from '../styles/shared';
-import { PageContext } from './post';
+} from "../styles/shared"
+import { PageContext } from "./post"
 
 const HomePosts = css`
   @media (min-width: 795px) {
@@ -64,35 +64,39 @@ const HomePosts = css`
       padding: 0 40px 30px;
     }
   }
-`;
+`
 
 export interface IndexProps {
   pageContext: {
-    currentPage: number;
-    numPages: number;
-  };
+    currentPage: number
+    numPages: number
+  }
   data: {
     logo: {
       childImageSharp: {
-        fixed: any;
-      };
-    };
+        fixed: any
+      }
+    }
     header: {
       childImageSharp: {
-        fluid: any;
-      };
-    };
+        fluid: any
+      }
+    }
     allMarkdownRemark: {
       edges: Array<{
-        node: PageContext;
-      }>;
-    };
-  };
+        node: PageContext
+      }>
+    }
+  }
 }
 
 const IndexPage: React.FC<IndexProps> = props => {
-  const width = props.data.header.childImageSharp.fluid.sizes.split(', ')[1].split('px')[0];
-  const height = String(Number(width) / props.data.header.childImageSharp.fluid.aspectRatio);
+  const width = props.data.header.childImageSharp.fluid.sizes
+    .split(", ")[1]
+    .split("px")[0]
+  const height = String(
+    Number(width) / props.data.header.childImageSharp.fluid.aspectRatio
+  )
 
   return (
     <IndexLayout css={HomePosts}>
@@ -109,8 +113,15 @@ const IndexPage: React.FC<IndexProps> = props => {
           property="og:image"
           content={`${config.siteUrl}${props.data.header.childImageSharp.fluid.src}`}
         />
-        {config.facebook && <meta property="article:publisher" content={config.facebook} />}
-        {config.googleSiteVerification && <meta name="google-site-verification" content={config.googleSiteVerification} />}
+        {config.facebook && (
+          <meta property="article:publisher" content={config.facebook} />
+        )}
+        {config.googleSiteVerification && (
+          <meta
+            name="google-site-verification"
+            content={config.googleSiteVerification}
+          />
+        )}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={config.title} />
         <meta name="twitter:description" content={config.description} />
@@ -122,7 +133,7 @@ const IndexPage: React.FC<IndexProps> = props => {
         {config.twitter && (
           <meta
             name="twitter:site"
-            content={`@${config.twitter.split('https://twitter.com/')[1]}`}
+            content={`@${config.twitter.split("https://twitter.com/")[1]}`}
           />
         )}
         <meta property="og:image:width" content={width} />
@@ -140,13 +151,13 @@ const IndexPage: React.FC<IndexProps> = props => {
               <SiteTitle>
                 {props.data.logo ? (
                   <img
-                    style={{ maxHeight: '45px' }}
+                    style={{ maxHeight: "45px" }}
                     src={props.data.logo.childImageSharp.fixed.src}
                     alt={config.title}
                   />
                 ) : (
-                    config.title
-                  )}
+                  config.title
+                )}
               </SiteTitle>
               <SiteDescription>{config.description}</SiteDescription>
             </SiteHeaderContent>
@@ -160,23 +171,26 @@ const IndexPage: React.FC<IndexProps> = props => {
                 // filter out drafts in production
                 return (
                   (post.node.frontmatter.draft !== true ||
-                    process.env.NODE_ENV !== 'production') && (
+                    process.env.NODE_ENV !== "production") && (
                     <PostCard key={post.node.fields.slug} post={post.node} />
                   )
-                );
+                )
               })}
             </div>
           </div>
         </main>
         {props.children}
-        <Pagination currentPage={props.pageContext.currentPage} numPages={props.pageContext.numPages} />
+        <Pagination
+          currentPage={props.pageContext.currentPage}
+          numPages={props.pageContext.numPages}
+        />
         <Footer />
       </Wrapper>
     </IndexLayout>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
   query blogPageQuery($skip: Int!, $limit: Int!) {
@@ -199,9 +213,9 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC },
-      filter: { frontmatter: { draft: { ne: true } } },
-      limit: $limit,
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
+      limit: $limit
       skip: $skip
     ) {
       edges {
@@ -242,4 +256,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
